ba653692f5174fa965b7b207fbdf68be2f79ad73
Anirudh Konidala <kidskodingclub@gmail.com>
2026-01-12 17:37:32 -0600

    fixing bugs in scraping aoc webpage

--- src/lib.rs
+++ src/lib.rs
@@ -49,17 +49,46 @@ pub async fn get_calendar_html(year: &str, session: &str) -> Result<String, AocE
 
 pub fn generate_svg(calendar_html: &str, css: &str, year: &str) -> String {
     format!(
-        r#"<svg xmlns="http://www.w3.org/2000/svg" width="800" height="500">
+        r##"<svg xmlns="http://www.w3.org/2000/svg" width="1000" height="800">
+            <rect width="100%" height="100%" fill="#0f0f23"/>
             <foreignObject width="100%" height="100%">
                 <div xmlns="http://www.w3.org/1999/xhtml">
-                    <style>{}</style>
-                    <div class="calendar-wrapper">
-                        <h2 class="year-title">Advent of Code {}</h2>
-                        <pre class="calendar">{}</pre>
+                    <style>
+                        @import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro&amp;display=swap");
+
+                        .calendar, .calendar * {{
+                            background: transparent !important;
+                            border: none !important;
+                            text-decoration: none !important;
+                            box-shadow: none !important;
+                            outline: none !important;
+                            color: inherit; /* Prevent links from being "Link Blue" */
+                        }}
+
+                        pre {{
+                            white-space: pre !important;
+                            margin: 0;
+                            padding: 20px;
+                            font-family: "Source Code Pro", monospace;
+                            color: #cccccc;
+                        }}
+
+                        .calendar-mark-complete, 
+                        .calendar-mark-verycomplete, 
+                        .calendar-color-s {{ 
+                            color: #ffff66 !important; 
+                            text-shadow: 0 0 5px #ffff66, 0 0 10px #ffff66 !important; 
+                        }}
+
+                        {}
+                    </style>
+                    <div class="calendar">
+                        <pre>{}</pre>
                     </div>
                 </div>
             </foreignObject>
-        </svg>"#,
-        css, year, calendar_html
+        </svg>"##,
+        css,
+        calendar_html
     )
 }
